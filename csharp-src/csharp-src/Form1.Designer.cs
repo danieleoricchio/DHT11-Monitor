d4acf1c951c7500b87d0dbc6dba42df541a8ea9b
danieleoricchio <lele13.do@gmail.com>
2021-09-06 15:52:01 +0200

    Push parte c#
    Push parte c# con l'interfaccia del sensore DHT11

---
+++ csharp-src/csharp-src/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace csharp_src
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.comboBoxPorte = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_umidita = new System.Windows.Forms.Label();
+            this.label_temperatura = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClose);
+            this.groupBox1.Controls.Add(this.btnOpen);
+            this.groupBox1.Controls.Add(this.comboBoxBaudRate);
+            this.groupBox1.Controls.Add(this.comboBoxPorte);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 137);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PORTE COM";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(120, 98);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(121, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(6, 98);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(107, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "OPEN";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Items.AddRange(new object[] {
+            "9600"});
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(113, 59);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBaudRate.TabIndex = 3;
+            // 
+            // comboBoxPorte
+            // 
+            this.comboBoxPorte.FormattingEnabled = true;
+            this.comboBoxPorte.Location = new System.Drawing.Point(113, 29);
+            this.comboBoxPorte.Name = "comboBoxPorte";
+            this.comboBoxPorte.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorte.TabIndex = 2;
+            this.comboBoxPorte.DropDown += new System.EventHandler(this.comboBoxPorte_DropDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PORTE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::csharp_src.Properties.Resources.dht11;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 156);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 266);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.label_umidita);
+            this.panel1.Controls.Add(this.label_temperatura);
+            this.panel1.Location = new System.Drawing.Point(265, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 70);
+            this.panel1.TabIndex = 2;
+            // 
+            // label_umidita
+            // 
+            this.label_umidita.AutoSize = true;
+            this.label_umidita.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_umidita.Location = new System.Drawing.Point(311, 24);
+            this.label_umidita.Name = "label_umidita";
+            this.label_umidita.Size = new System.Drawing.Size(151, 25);
+            this.label_umidita.TabIndex = 1;
+            this.label_umidita.Text = "Umiditá = __%";
+            // 
+            // label_temperatura
+            // 
+            this.label_temperatura.AutoSize = true;
+            this.label_temperatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_temperatura.Location = new System.Drawing.Point(26, 24);
+            this.label_temperatura.Name = "label_temperatura";
+            this.label_temperatura.Size = new System.Drawing.Size(178, 24);
+            this.label_temperatura.TabIndex = 0;
+            this.label_temperatura.Text = "Temperatura = __°C";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(265, 98);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Temperatura";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Umiditá";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(523, 340);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Grafico Temperatura e Umiditá";
+            this.chart1.Titles.Add(title1);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DHT11 Monitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form1_FormClosing);
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.ComboBox comboBoxPorte;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label_umidita;
+        private System.Windows.Forms.Label label_temperatura;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ csharp-src/csharp-src/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_src
+{
+    public partial class form1 : Form
+    {
+        double temperatura = 0, umidita = 0;
+        bool checkData = false;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+
+            chart1.Series["Temperatura"].Points.AddXY(1, 1);
+            chart1.Series["Umiditá"].Points.AddXY(1, 1);
+        }
+
+        private void comboBoxPorte_DropDown(object sender, EventArgs e)
+        {
+            string[] listaPorte = SerialPort.GetPortNames();
+            comboBoxPorte.Items.Clear();
+            comboBoxPorte.Items.AddRange(listaPorte);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBoxPorte.Text;
+                serialPort1.BaudRate = Convert.ToInt32(comboBoxBaudRate.Text);
+                serialPort1.Open();
+                
+                btnOpen.Enabled = false;
+                btnClose.Enabled = true;
+
+                chart1.Series["Temperatura"].Points.Clear();
+                chart1.Series["Umiditá"].Points.Clear();
+
+                MessageBox.Show("Connessione effettuata!");
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+
+                btnOpen.Enabled = true;
+                btnClose.Enabled = false;
+
+                MessageBox.Show("Disconnessione effettuata!");
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadTo("\n");
+            Data_Parsing(data);
+            this.BeginInvoke(new EventHandler(ShowData));
+        }
+
+        private void ShowData(object sender, EventArgs e)
+        {
+            if (checkData)
+            {
+                label_temperatura.Text = string.Format("Temperatura = {0} °C", temperatura.ToString());
+                label_umidita.Text = string.Format("Umiditá = {0} %", umidita.ToString());
+
+                chart1.Series["Temperatura"].Points.Add(temperatura);
+                chart1.Series["Umiditá"].Points.Add(umidita);
+            }
+        }
+
+        private void Data_Parsing(string data)
+        {
+            char carattereIniziale = (char)data.IndexOf("@");
+            char indexA = (char)data.IndexOf("A");
+            char indexB = (char)data.IndexOf("B");
+
+            if(indexA != -1 && indexB != -1 && carattereIniziale != -1)
+            {
+                try
+                {
+                    string str_temperatura = data.Substring(carattereIniziale + 1, (indexA - carattereIniziale) - 1);
+                    string str_umidita = data.Substring(indexA + 1, (indexB - indexA) - 1);
+
+                    temperatura = Convert.ToDouble(str_temperatura);
+                    umidita = Convert.ToDouble(str_umidita);
+
+                    checkData = true;
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+            else
+            {
+                checkData = false;
+            }
+        }
+    }
+}
