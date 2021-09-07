61958f43d1dc09cf09dc32c015200ee51e19f955
danieleoricchio <lele13.do@gmail.com>
2021-09-07 17:35:53 +0200

    Aggiunto lcd

--- arduino-src/arduino-src.ino
+++ arduino-src/arduino-src.ino
@@ -1,12 +1,15 @@
 #include "DHT.h"
+#include <LiquidCrystal.h>
 
 #define DHTPIN 8
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16,2);
   dht.begin();
 }
 
@@ -17,9 +20,15 @@ void loop() {
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    Serial.println("Impossibile leggere dal sensore dht");
     return;
   }
+
+  lcd.setCursor(0,0);
+  lcd.print("Temp: " + (String)t + char(223) + "C");
+
+  lcd.setCursor(0,1);
+  lcd.print("Umidita: " + (String)h + " %");
 
   Serial.print("@");
   Serial.print(t); Serial.print("A");
